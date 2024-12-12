746f51202253785cec36ccf88bd82a15eaa5f868
Tomasz Tworek <tomasztworek27@gmail.com>
2024-12-13 00:05:44 +0100

    Initial commit

---
+++ src/main/java/pl/personal/ToDoDeep/ToDoDeepApplication.java
@@ -0,0 +1,13 @@
+package pl.personal.ToDoDeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoDeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoDeepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/personal/ToDoDeep/ToDoDeepApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.personal.ToDoDeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoDeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
